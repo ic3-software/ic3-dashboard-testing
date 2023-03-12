da49d21142ae98580446d67c1a5bca36cf341d6b
david <david.alvarez@iccube.com>
2023-03-12 09:34:48 +0100

    Add github action for recording errors

---
+++ cypress.config-github-recording.ts
@@ -0,0 +1,31 @@
+import {defineConfig} from "cypress";
+
+export default defineConfig({
+
+    e2e: {
+
+        supportFile: "cypress/support/e2e.ts",
+
+        specPattern: [
+            "**/TableLocalState.spec.ts"
+        ],
+
+        excludeSpecPattern: [
+            "**/ic3/**/*.spec.ts",
+            "**/DatePickerRangeBehavior.spec.ts",
+        ],
+
+        retries: {
+            runMode: 2,
+            openMode: 2,
+        },
+
+        reporter: "Min",
+
+        viewportWidth: 1600,
+        viewportHeight: 1200,
+
+        video: false,
+
+    }
+})
