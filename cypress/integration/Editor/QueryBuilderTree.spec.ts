b71b3eaeba7a8edcfaa55e9b9f8925396543eb96
tom-vd-b <tom.vandenberg@inside-vision.com>
2023-05-03 11:48:56 +0200

    Test query tree not closing when pressing apply.

--- cypress/integration/Editor/QueryBuilderTree.spec.ts
+++ cypress/integration/Editor/QueryBuilderTree.spec.ts
@@ -1,6 +1,55 @@
+
 export {};
 
-describe("Editor/Zoom in out", () => {
+enum SchemaBrowserMdxEntityDataType {
+
+    SCHEMA = "SA",
+    CUBE = "CE",
+    DIMENSION = "DN",
+    HIERARCHY = "HY",
+    HIERARCHY_MEASURE = "HYME",
+    LEVEL = "LL",
+    MEMBER = "MR",
+    MEMBER_PROPERTY = "PR",
+    MEMBER_PROPERTY_ROOT = "PRR",
+    TUPLE = "TE",
+    MEASURE_GROUP = "MP",
+    MEASURE_FOLDER = "MF",
+    MEASURE = "ME",
+    REPORT_CALC_MEASURE = "RCME",
+    REPORT_FOLDER = "RPF",
+    REPORT = "RP",
+    REPORT_SNAPSHOT = "RPS",
+    OTHER = "OT",
+
+    /**
+     * Experimental DataSets
+     */
+    DATASET_PROPERTY_ROOT = "DS_PR",
+    DATASET_MEMBER_COLUMN = "DS_MRCOL",
+    DATASET_MEASURE_COLUMN = "DS_MECOL",
+
+    /**
+     * ic3report client side only !
+     */
+    EVENTS = "EVTS",
+    EVENT = "EVT",
+    SET_FOLDER = "FOLDER_SET",
+    SET = "SET",
+    HIERARCHY_FOLDER = "HIERARCHY_FOLDER",
+    LEVEL_GROUP = "LEVEL_GROUP"
+
+}
+
+function schemaBrowserExpandNode(type: SchemaBrowserMdxEntityDataType, caption: string) {
+
+    cy.get('.MuiDrawer-root .ic3WidgetEditorQueryPanel-Hierarchies div[data-cy-type="' + type + '"][data-cy="' + caption + '"] button')
+        .click({multiple: true})
+    ;
+
+}
+
+describe("Query Builder Tree", () => {
 
     beforeEach(() => {
         cy.login();
@@ -50,6 +99,18 @@ describe("Editor/Zoom in out", () => {
         cy.widgetEditorMdxTreeHasNode("Color Measures");
         cy.widgetEditorMdxTreeHasNode("Country - Visible");
 
+    });
+
+    it("not closing on apply", () => {
+
+        cy.widgetEditorOpen("ww0");
+        cy.widgetEditorChangeTab("tab-query");
+
+        schemaBrowserExpandNode(SchemaBrowserMdxEntityDataType.DIMENSION, "Geography");
+        schemaBrowserExpandNode(SchemaBrowserMdxEntityDataType.HIERARCHY, "Geography");
+        cy.widgetEditorApply();
+
+        cy.get("div.ic3QueryBuilderNode-itemText[title='Country (31)']").should("be.visible");
     });
 
 });
