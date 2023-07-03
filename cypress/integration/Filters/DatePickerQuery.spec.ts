836bafcf417a60917d80d2d818feff14ab20b5c0
david <david.alvarez@iccube.com>
2023-07-03 16:42:05 +0200

    [DatePicker] more examples on query

--- cypress/integration/Filters/DatePickerQuery.spec.ts
+++ cypress/integration/Filters/DatePickerQuery.spec.ts
@@ -6,7 +6,7 @@ describe("Filters/DatePicker Query", () => {
     beforeEach(() => {
         cy.login();
         cy.openViewerTestReport("Filters/DatePicker Query", true, false);
-        cy.waitForQueryCount(3);
+        cy.waitForQueryCount(4);
     });
 
     it("ww0: Date Picker - hierarchy", () => {
@@ -22,6 +22,16 @@ describe("Filters/DatePicker Query", () => {
     it("ww2: Date Picker - multiple hierarchies", () => {
         cy.assertDatePickerRangeFrom("ww2", "5 Aug 2019");
         cy.assertDatePickerRangeTo("ww2", "4 Jan 2025");
+    });
+
+    it("ww3: Date Picker - two dates levels", () => {
+        cy.assertDatePickerRangeFrom("ww3", "1 Jan 2020");
+        cy.assertDatePickerRangeTo("ww3", "31 Mar 2023");
+    });
+
+    it("ww4: Date Picker - csv", () => {
+        cy.assertDatePickerRangeFrom("ww4", "1 Jan 2020");
+        cy.assertDatePickerRangeTo("ww4", "31 Jan 2020");
     });
 
 });
