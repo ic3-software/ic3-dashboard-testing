a9ff22431c207785b45208f7a78437b1cf5caf97
marc-polizzi <marc.polizzi@gmail.com>
2023-09-11 18:33:41 +0200

    pivot table drilldown on zoom : drilldown before zooming

--- cypress/integration/Tables/PivotTableDrilldownOnZoom.spec.ts
+++ cypress/integration/Tables/PivotTableDrilldownOnZoom.spec.ts
@@ -34,4 +34,34 @@ describe("Tables/Pivot Table Drilldown on Zoom", () => {
 
     })
 
+    it("zoom expanded", () => {
+
+        const widgetId = "ww0";
+
+        cy.waitForQueryCount(1);
+
+        // Same as 'zoom' but before opening the zoom
+
+        cy.getWidget(widgetId).find(".ic3WidgetBox-content .ic3-pt-rows .ic3-pt-row")
+            .should("have.length", 5)
+        cy.getWidget(widgetId).find(`.ic3WidgetBox-content .ic3-pt-left-header div[data-vr='1'][data-vc='0'] svg`)
+            .click()
+        cy.getWidget(widgetId).find(".ic3WidgetBox-content .ic3-pt-rows .ic3-pt-row")
+            .should("have.length", 5 + 4)
+        cy.getWidget(widgetId).find(`.ic3WidgetBox-content .ic3-pt-left-header div[data-vr='3'][data-vc='0'] svg`)
+            .click()
+        cy.getWidget(widgetId).find(".ic3WidgetBox-content .ic3-pt-rows .ic3-pt-row")
+            .should("have.length", 5 + 4 + 3)
+
+        cy.waitForQueryCount(1 + 2);
+        cy.assertPivotTableRowCount(widgetId, 5 + 4 + 3)
+
+        cy.clickUserMenuZoom(widgetId);
+        cy.waitForQueryCount(1 + 2);
+
+        cy.getZoomedWidget(widgetId).find(".ic3WidgetBox-content .ic3-pt-rows .ic3-pt-row")
+            .should("have.length", 5 + 4 + 3)
+
+    })
+
 });
