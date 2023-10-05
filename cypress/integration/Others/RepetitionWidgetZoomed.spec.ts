29652aaec98911315847a92cf6c657c90c2bf439
marc-polizzi <marc.polizzi@gmail.com>
2023-10-05 05:43:57 +0200

    repetition-widget: revert expected values

--- cypress/integration/Others/RepetitionWidgetZoomed.spec.ts
+++ cypress/integration/Others/RepetitionWidgetZoomed.spec.ts
@@ -8,17 +8,19 @@ describe("Others/RepetitionWidgetRendering", () => {
 
     it("Test we get what we expect", () => {
 
-        cy.assertWidgetDetails(0, "ww0", 207.891, 150, 1069.1875, 786);
+        // Expected values ok when running from a Github action (failing locally).
+
+        cy.assertWidgetDetails(0, "ww0", 207.9, 150, 1069.1875, 786);
 
         cy.assertWidgetDetails(0, "ww0~:Rep-ww1-R:0-C:0", 25, 18, 482.109375, 350);
-        cy.assertWidgetDetails(0, "ww0~:Rep-ww2-R:0-C:0", 532.109, 18, 512.078125, 175);
-        cy.assertWidgetDetails(0, "ww0~:Rep-ww3-R:0-C:0", 532.109, 218, 512.078125, 150);
+        cy.assertWidgetDetails(0, "ww0~:Rep-ww2-R:0-C:0", 532.112, 18, 512.078125, 175);
+        cy.assertWidgetDetails(0, "ww0~:Rep-ww3-R:0-C:0", 532.112, 218, 512.078125, 150);
 
         const topDist = 386 - 18;
-        cy.assertWidgetDetails(0, "ww0~:Rep-ww1-R:1-C:0", 25, 18 + topDist, 482.109375, 350);
-        cy.assertWidgetDetails(0, "ww0~:Rep-ww2-R:1-C:0", 532.109, 18 + topDist, 512.078125, 175);
-        cy.assertWidgetDetails(0, "ww0~:Rep-ww3-R:1-C:0", 532.109, 218 + topDist, 512.078125, 150);
 
+        cy.assertWidgetDetails(0, "ww0~:Rep-ww1-R:1-C:0", 25, 18 + topDist, 482.109375, 350);
+        cy.assertWidgetDetails(0, "ww0~:Rep-ww2-R:1-C:0", 532.112, 18 + topDist, 512.078125, 175);
+        cy.assertWidgetDetails(0, "ww0~:Rep-ww3-R:1-C:0", 532.112, 218 + topDist, 512.078125, 150);
 
     });
 
