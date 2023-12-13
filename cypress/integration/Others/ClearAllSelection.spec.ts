9173e3c8b044264684ff496f1197ccc82e3fff39
tom-vd-b <tom.vandenberg@inside-vision.com>
2023-12-13 15:44:55 +0100

    Test clearing all selection and resetting selection in complex scenario.

---
+++ cypress/integration/Others/ClearAllSelection.spec.ts
@@ -0,0 +1,48 @@
+import {assertButtonsSelection} from "../Filters/FilterUtils";
+
+export {};
+
+
+describe("Others/clear all selection", () => {
+
+    beforeEach(() => {
+        cy.login();
+        cy.openViewerTestReport("Others/clear all selection", true, false);
+        cy.waitForQueryCount(3 /* 3 embedded data source */);
+    });
+
+    it("Test app notification to clear selection is working", () => {
+
+        // Start with buttons selected
+        cy.selectButton("ww1", "2020 Feb");
+        cy.assertButtonsSelected("ww1", ["2020 Feb"]);
+        cy.selectButton("ww2", "2022");
+        cy.assertButtonsSelected("ww2", ["2022"]);
+
+        // Now clear all selection. Expected to clear both ww1 and ww2.
+        cy.selectButton("ww3", "Clear Selection");
+
+        // Buttons should be in default state: no selection.
+        cy.assertButtonsSelected("ww1", []);
+        cy.assertButtonsSelected("ww2", []);
+
+    });
+
+    it("Test app notification to reset selection is working", () => {
+
+        // Start with buttons selected
+        cy.selectButton("ww1", "2020 Feb");
+        cy.assertButtonsSelected("ww1", ["2020 Feb"]);
+        cy.selectButton("ww2", "2022");
+        cy.assertButtonsSelected("ww2", ["2022"]);
+
+        // Now clear all selection. Expected to clear both ww1 and ww2.
+        cy.selectButton("ww3", "Reset Selection");
+
+        // Buttons should be in default state: no selection.
+        cy.assertButtonsSelected("ww1", []);
+        cy.assertButtonsSelected("ww2", []);
+
+    });
+
+});
---
+++ cypress/integration/Others/ResetSelection.spec.ts
@@ -0,0 +1,31 @@
+import {assertButtonsSelection} from "../Filters/FilterUtils";
+
+export {};
+
+
+describe("Others/reset selection", () => {
+
+    beforeEach(() => {
+        cy.login();
+        cy.openViewerTestReport("Others/reset selection", true, false);
+        cy.waitForQueryCount(3 /* 3 embedded data source */);
+    });
+
+    it("Test app notification to reset selection is working with data dependent initial selection ", () => {
+
+        // Start with buttons selected
+        cy.selectButton("ww1", "2020 Feb");
+        cy.assertButtonsSelected("ww1", ["2020 Feb"]);
+        cy.selectButton("ww2", "2022");
+        cy.assertButtonsSelected("ww2", ["2022"]);
+
+        // Now clear all selection. Expected to clear both ww1 and ww2.
+        cy.selectButton("ww3", "Reset Selection");
+
+        // Buttons should be in default state: no selection.
+        cy.assertButtonsSelected("ww1", ["2020 Mar"]);  // Should be the default selection
+        cy.assertButtonsSelected("ww2", []);
+
+    });
+
+});
