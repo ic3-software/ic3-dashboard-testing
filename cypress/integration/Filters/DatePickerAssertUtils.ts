af13aa3726bbf5265fd79686c21adc236f6aa216
tom-vd-b <tom.vandenberg@inside-vision.com>
2025-01-21 16:28:52 +0100

    Add range picker and date picker edge case tests.

--- cypress/integration/Filters/DatePickerAssertUtils.ts
+++ cypress/integration/Filters/DatePickerAssertUtils.ts
@@ -1,6 +1,8 @@
 import {DateUtils} from "./DateUtils";
 
-export function assertDates(widgetId: string, eventWidgetId: string, tableWidgetId: string, dateF: string | null, eventF: string | null, mdxF: Date | null, dateT: string | null, eventT: string | null, mdxT: Date | null) {
+export function assertDates(widgetId: string, eventWidgetId: string, tableWidgetId: string, dateF: string | null,
+                            eventF: string | null, mdxF: Date | null, dateT: string | null, eventT: string | null,
+                            mdxT: Date | null, tableF?: string, tableT?: string) {
 
     cy.assertDatePickerRangeFrom(widgetId, dateF);
     cy.assertDatePickerRangeTo(widgetId, dateT);
@@ -15,20 +17,34 @@ export function assertDates(widgetId: string, eventWidgetId: string, tableWidget
 
         cy.getWidget(tableWidgetId, "data-cy-waiting");
 
-    } else if (eventF) {
+    } else {
 
-        if (eventF.startsWith("0")) {
-            cy.assertTableValue(tableWidgetId, 0, 0, eventF.substring(1));
-        } else {
-            cy.assertTableValue(tableWidgetId, 0, 0, eventF);
+        const tableFromValueToAssert = tableF ?? eventF;
+
+        if (tableFromValueToAssert) {
+
+            if (tableFromValueToAssert.startsWith("0")) {
+                cy.assertTableValue(tableWidgetId, 0, 0, tableFromValueToAssert.substring(1));
+            } else {
+                cy.assertTableValue(tableWidgetId, 0, 0, tableFromValueToAssert);
+            }
+
         }
 
-    } else if (eventT) {
+        const tableToValueToAssert = tableT ?? eventT;
 
-        if (eventT.startsWith("0")) {
-            cy.assertTableValue(tableWidgetId, 1, 0, eventT.substring(1));
-        } else {
-            cy.assertTableValue(tableWidgetId, 1, 0, eventT);
+        if (tableToValueToAssert) {
+
+            if (tableToValueToAssert.startsWith("0")) {
+                cy.assertTableValue(tableWidgetId, 1, 0, tableToValueToAssert.substring(1));
+            } else {
+                cy.assertTableValue(tableWidgetId, 1, 0, tableToValueToAssert);
+            }
+
+        }
+
+        if (tableFromValueToAssert == "" && tableToValueToAssert == "") {
+            cy.assertWidgetNoData(tableWidgetId);
         }
 
     }
@@ -36,7 +52,8 @@ export function assertDates(widgetId: string, eventWidgetId: string, tableWidget
 
 }
 
-export function assertDate(widgetId: string, eventWidgetId: string, tableWidgetId: string, date: string | null, event: string | null, mdx: string | null) {
+export function assertDate(widgetId: string, eventWidgetId: string, tableWidgetId: string, date: string | null,
+                           event: string | null, mdx: string | null, noData?: boolean) {
 
     cy.assertDatePicker(widgetId, date);
 
@@ -49,7 +66,9 @@ export function assertDate(widgetId: string, eventWidgetId: string, tableWidgetI
 
     } else {
 
-        if (event.startsWith("0")) {
+        if (noData) {
+            cy.assertWidgetNoData(tableWidgetId);
+        } else if (event.startsWith("0")) {
             cy.assertTableValue(tableWidgetId, 0, 0, event.substr(1));
         } else {
             cy.assertTableValue(tableWidgetId, 0, 0, event);
---
+++ cypress/integration/Filters/DatePickerEdgeCasesII.spec.ts
@@ -0,0 +1,157 @@
+import {DateUtils} from "./DateUtils";
+import {assertDate, assertDates} from "./DatePickerAssertUtils";
+
+export {};
+
+describe("Filters/DatePicker Edge Cases II", () => {
+
+    beforeEach(() => {
+        cy.login();
+        cy.openViewerTestReport("Filters/DatePicker Edge Cases II", true, false);
+        cy.waitForQueryCount(4);
+    });
+
+    it("ww0: Date Picker - no data if outside range", () => {
+
+        const widgetId = "ww0";
+        const eventWidgetId = "ww1";
+        const tableWidgetId = "ww2";
+
+        assertDate(widgetId, eventWidgetId, tableWidgetId, null, null, null, true);
+
+        // The range of allowed dates goes from 1 jan 2018 to 15 Sep 2022.
+        cy.selectDatePickerFromInput(widgetId, "31 Dec 2017");
+        assertDate(widgetId, eventWidgetId, tableWidgetId, "31 Dec 2017", "31 Dec 2017", DateUtils.mdx("DateTime(2017,12,31)"), true);
+        cy.selectDatePickerFromInput(widgetId, "01 Jan 2018");
+        assertDate(widgetId, eventWidgetId, tableWidgetId, "01 Jan 2018", "01 Jan 2018", DateUtils.mdx("DateTime(2018,1,1)"), false);
+        cy.selectDatePickerFromInput(widgetId, "02 Jan 2018");
+        assertDate(widgetId, eventWidgetId, tableWidgetId, "02 Jan 2018", "02 Jan 2018", DateUtils.mdx("DateTime(2018,1,2)"), false);
+
+        cy.selectDatePickerFromInput(widgetId, "14 Sep 2022");
+        assertDate(widgetId, eventWidgetId, tableWidgetId, "14 Sep 2022", "14 Sep 2022", DateUtils.mdx("DateTime(2022,9,14)"), false);
+        cy.selectDatePickerFromInput(widgetId, "15 Sep 2022");
+        assertDate(widgetId, eventWidgetId, tableWidgetId, "15 Sep 2022", "15 Sep 2022", DateUtils.mdx("DateTime(2022,9,15)"), false);
+        cy.selectDatePickerFromInput(widgetId, "16 Sep 2022");
+        assertDate(widgetId, eventWidgetId, tableWidgetId, "16 Sep 2022", "16 Sep 2022", DateUtils.mdx("DateTime(2022,9,16)"), true);
+
+    });
+
+    it("ww3: Date Picker - selecting edge of range", () => {
+
+        const widgetId = "ww3";
+        const eventWidgetId = "ww4";
+        const tableWidgetId = "ww5";
+
+        assertDate(widgetId, eventWidgetId, tableWidgetId, null, null, null, true);
+
+        // The range of allowed dates goes from 1 jan 2018 to 15 Sep 2022.
+        cy.selectDatePickerFromInput(widgetId, "31 Dec 2017");
+        // Keeps old state since 31 Dec 2017 is not allowed.
+        assertDate(widgetId, eventWidgetId, tableWidgetId, "31 Dec 2017", null, null);
+        cy.selectDatePickerFromInput(widgetId, "01 Jan 2018");
+        assertDate(widgetId, eventWidgetId, tableWidgetId, "01 Jan 2018", "01 Jan 2018", DateUtils.mdx("DateTime(2018,1,1)"), false);
+        cy.selectDatePickerFromInput(widgetId, "02 Jan 2018");
+        assertDate(widgetId, eventWidgetId, tableWidgetId, "02 Jan 2018", "02 Jan 2018", DateUtils.mdx("DateTime(2018,1,2)"), false);
+
+        cy.selectDatePickerFromInput(widgetId, "14 Sep 2022");
+        assertDate(widgetId, eventWidgetId, tableWidgetId, "14 Sep 2022", "14 Sep 2022", DateUtils.mdx("DateTime(2022,9,14)"), false);
+        cy.selectDatePickerFromInput(widgetId, "15 Sep 2022");
+        assertDate(widgetId, eventWidgetId, tableWidgetId, "15 Sep 2022", "15 Sep 2022", DateUtils.mdx("DateTime(2022,9,15)"), false);
+        cy.selectDatePickerFromInput(widgetId, "16 Sep 2022");
+        // Keeps old state since 16 Sep 2022 is not allowed.
+        assertDate(widgetId, eventWidgetId, tableWidgetId, "16 Sep 2022", "15 Sep 2022", DateUtils.mdx("DateTime(2022,9,15)"), false);
+
+    });
+
+    it("ww6: Range Picker - no data if outside range", () => {
+
+        const widgetId = "ww6";
+        const eventWidgetId = "ww7";
+        const tableWidgetId = "ww8";
+
+        assertDates(widgetId, eventWidgetId, tableWidgetId,
+            null, null, null,
+            null, null, null
+        );
+
+        // Data range goes from 1 jan 2018 to 15 Sep 2022.
+        // Allowed range goes from 1 jan 2017 to 15 Sep 2023.
+
+        {  // Testcase large range than data
+            cy.selectDatePickerRangeFromFromInput(widgetId, "01 Jan 2017");
+            cy.selectDatePickerRangeToFromInput(widgetId, "15 Sep 2023");
+            assertDates(widgetId, eventWidgetId, tableWidgetId,
+                "01 Jan 2017", "01 Jan 2017", new Date(2017, 0, 1),
+                "15 Sep 2023", "15 Sep 2023", new Date(2023, 8, 15),
+                "01 Jan 2018", "15 Sep 2022"
+            );
+        }
+
+        {  // Testcase range outside data before
+            cy.selectDatePickerRangeFromFromInput(widgetId, "01 Jan 2017");
+            cy.selectDatePickerRangeToFromInput(widgetId, "02 Jan 2017");
+            assertDates(widgetId, eventWidgetId, tableWidgetId,
+                "01 Jan 2017", "01 Jan 2017", new Date(2017, 0, 1),
+                "02 Jan 2017", "02 Jan 2017", new Date(2017, 0, 2),
+                "", ""
+            );
+        }
+
+        {  // Testcase range outside data after
+            cy.selectDatePickerRangeFromFromInput(widgetId, "14 Sep 2023");
+            cy.selectDatePickerRangeToFromInput(widgetId, "15 Sep 2023");
+            assertDates(widgetId, eventWidgetId, tableWidgetId,
+                "14 Sep 2023", "14 Sep 2023", new Date(2023, 8, 14),
+                "15 Sep 2023", "15 Sep 2023", new Date(2023, 8, 15),
+                "", ""
+            );
+        }
+
+        {  // Testcase range outside to just within range before
+            cy.selectDatePickerRangeFromFromInput(widgetId, "01 Jan 2017");
+            cy.selectDatePickerRangeToFromInput(widgetId, "01 Jan 2018");
+            assertDates(widgetId, eventWidgetId, tableWidgetId,
+                "01 Jan 2017", "01 Jan 2017", new Date(2017, 0, 1),
+                "01 Jan 2018", "01 Jan 2018", new Date(2018, 0, 1),
+                "01 Jan 2018", "01 Jan 2018"
+            );
+        }
+
+        {  // Testcase range outside to just within range after
+            cy.selectDatePickerRangeFromFromInput(widgetId, "15 Sep 2022");
+            cy.selectDatePickerRangeToFromInput(widgetId, "15 Sep 2023");
+            assertDates(widgetId, eventWidgetId, tableWidgetId,
+                "15 Sep 2022", "15 Sep 2022", new Date(2022, 8, 15),
+                "15 Sep 2023", "15 Sep 2023", new Date(2023, 8, 15),
+                "15 Sep 2022", "15 Sep 2022"
+            );
+        }
+
+    });
+
+    it("ww9: Range Picker - selecting edge of range", () => {
+
+        const widgetId = "ww9";
+        const eventWidgetId = "ww10";
+        const tableWidgetId = "ww11";
+
+        assertDates(widgetId, eventWidgetId, tableWidgetId,
+            null, null, null,
+            null, null, null
+        );
+
+        // Data range goes from 1 jan 2018 to 15 Sep 2022.
+        // Allowed range goes from 1 jan 2017 to 15 Sep 2023.
+
+        {  // Testcase exact data range
+            cy.selectDatePickerRangeFromFromInput(widgetId, "01 Jan 2018");
+            cy.selectDatePickerRangeToFromInput(widgetId, "15 Sep 2022");
+            assertDates(widgetId, eventWidgetId, tableWidgetId,
+                "01 Jan 2018", "01 Jan 2018", new Date(2018, 0, 1),
+                "15 Sep 2022", "15 Sep 2022", new Date(2022, 8, 15)
+            );
+        }
+
+    });
+
+});
