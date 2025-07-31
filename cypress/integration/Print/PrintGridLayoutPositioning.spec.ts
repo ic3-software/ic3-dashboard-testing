1d81c52ec53cc7bbfe232902bca37f03667f0b0f
tom-vd-b <tom.vandenberg@inside-vision.com>
2025-07-31 11:02:28 +0200

    fix grid layout w/ inivisible widgets

---
+++ cypress/integration/Print/PrintGridLayoutPositioning.spec.ts
@@ -0,0 +1,33 @@
+describe("Print/printGridLayoutI", () => {
+
+    beforeEach(() => {
+        cy.login();
+    });
+
+    it("Assert grid layout expands the rows correctly with invisible widgets", () => {
+
+        cy.openPrintInBrowserTestReport("Print/printGridLayoutI", undefined, undefined, "portrait");
+        cy.waitForQueryCount(0);
+
+        // sizes to full width because ww14 and ww13 are invisible
+        assertSizing("ww12", 1121);
+
+        // divide space of invisible widgets equally for this row
+        assertSizing("ww2", 320);
+        assertSizing("ww7", 275);
+        assertSizing("ww8", 229);
+        assertSizing("ww9", 275);
+
+    });
+
+});
+
+function assertSizing(widget: string, expected: number) {
+    // print scale = 0.64
+    // correct size = expected * 0.64
+    cy.getWidget(widget)
+        .should('have.css', 'width').should(width => {
+        expect(width).to.include('px');
+        expect(parseInt(width, 10)).to.be.within((expected - 3) * 0.64, (expected + 3) * 0.64);  // Adres rounding issues.
+    });
+}
