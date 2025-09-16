573051efb51cc22190713e0d586f58d5fb142d51
david-ic3 <david.alvarez@iccube.com>
2025-09-16 11:24:52 +0200

    fix test

--- cypress/integration/Selection/TableAndChartSelection.spec.ts
+++ cypress/integration/Selection/TableAndChartSelection.spec.ts
@@ -18,16 +18,19 @@ describe("Selection/Filter By and Selection", () => {
         const event1 = "ww3";
 
         cy.clickTableCell(table1, 0, 0);  // 2018
+        cy.waitForQueryCount(3);
         cy.assertEventValue(event1, "2018");
         cy.assertTableSingleRowSelected(table1, 0, 4);
 
         cy.selectSingleChartBarInGroup(chart0, 0, 1);
+        cy.waitForQueryCount(4);
         cy.assertEventValue(event0, "Europe");
         cy.assertSelectedSingleChartBarInGroup(chart0, 0, 1);
 
 
         // Click asia
         cy.selectSingleChartBarInGroup(chart0, 0, 0);
+        cy.waitForQueryCount(5);
         cy.assertEventValue(event0, "Asia");
         cy.assertSelectedSingleChartBarInGroup(chart0, 0, 0);
         cy.assertTableSingleRowSelected(table1, 0, 4);
@@ -35,6 +38,7 @@ describe("Selection/Filter By and Selection", () => {
 
         // click in the table and check that the selection in the chart is stil there
         cy.clickTableCell(table1, 1, 0);  // 2019
+        cy.waitForQueryCount(6);
         cy.assertEventValue(event1, "2019");
         cy.assertTableSingleRowSelected(table1, 1, 4);
         cy.assertSelectedSingleChartBarInGroup(chart0, 0, 1);
@@ -42,11 +46,13 @@ describe("Selection/Filter By and Selection", () => {
 
         // unselect chart
         cy.selectSingleChartBarInGroup(chart0, 0, 1);
+        cy.waitForQueryCount(7);
         cy.assertEventValue(event0, "");
         cy.assertTableSingleRowSelected(table1, 1, 4);
 
 
         cy.clickTableCell(table1, 1, 0);  // 2019
+        cy.waitForQueryCount(8);
         cy.assertEventValue(event1, "");
         cy.assertTableSingleRowSelected(table1, -1, 4);
 
