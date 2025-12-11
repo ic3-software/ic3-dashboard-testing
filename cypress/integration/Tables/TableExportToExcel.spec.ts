24bac8e54e5b211c6df2b6e1e05ecb8119728331
tom-vd-b <tom.vandenberg@inside-vision.com>
2025-12-11 16:03:27 +0100

    export excel test

--- cypress/integration/Tables/TableExportToExcel.spec.ts
+++ cypress/integration/Tables/TableExportToExcel.spec.ts
@@ -34,14 +34,19 @@ describe("Tables/ExportToExcel", () => {
 
             readXlsxFile(blob).then((rows: Row[]) => {
 
-                expect(rows.length).to.eq(4)
-                expect(rows[0].length).to.eq(4 + 1)
-                expect(rows[1][2]).to.eq('Personal')
-                expect(rows[1][0]).to.eq(148250)
-                expect(rows[1][3]).to.eq(250)
-                expect(rows[1][4]).to.eq(250)
-                expect(rows[2][2]).to.eq('Silver')
-                expect(rows[3][2]).to.eq('Server')
+                console.log(rows);
+
+                const rowOffset = 9;
+                const colOffset = 1;
+
+                expect(rows.length).to.eq(13)
+                expect(rows[rowOffset + 0].length).to.eq(4 + 1)
+                expect(rows[rowOffset + 1][colOffset + 0]).to.eq('Personal')
+                expect(rows[rowOffset + 1][colOffset + 1]).to.eq(148250)
+                expect(rows[rowOffset + 1][colOffset + 2]).to.eq(250)
+                expect(rows[rowOffset + 1][colOffset + 3]).to.eq(250)
+                expect(rows[rowOffset + 2][colOffset + 0]).to.eq('Silver')
+                expect(rows[rowOffset + 3][colOffset + 0]).to.eq('Server')
 
             });
 
