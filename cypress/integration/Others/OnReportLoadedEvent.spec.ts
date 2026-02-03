3f9bc7321e4f55600ff198c1389f8685f7535d72
david-ic3 <david.alvarez@iccube.com>
2026-02-03 14:58:44 +0100

    add test onReportLoadedEvent

---
+++ cypress/integration/Others/OnReportLoadedEvent.spec.ts
@@ -0,0 +1,47 @@
+// const europeanCountries = [
+//     "Belgium",
+//     "France",
+//     "Germany",
+//     "Italy",
+//     "Netherlands",
+//     "Poland",
+//     "Russia",
+// ];
+//
+// const northAmericaCountries = ["Canada", "Mexico", "United States"];
+
+
+describe("Others/onReportLoadedEvent", () => {
+
+    beforeEach(() => {
+        cy.login();
+        cy.openEditorTestReport("Others/onReportLoadedEvent");
+        cy.waitForPrintStatus()
+
+    });
+
+    it("test MDX filter", () => {
+
+        cy.assertTableColCount("ww0", 2);
+
+        cy.assertEventWithText("ww1", "myEvent" , "onReportLoadedEvent")
+        cy.assertEventWithText("ww1", "id1" , "1")
+        cy.assertEventWithText("ww1", "id2" , "1")
+
+        cy.refreshDashboard();
+        cy.wait(100);
+        cy.waitForPrintStatus();
+        cy.assertEventWithText("ww1", "id1" , "2");
+        cy.assertEventWithText("ww1", "id2" , "1")
+
+        cy.refreshDashboard();
+        cy.wait(100);
+        cy.waitForPrintStatus();
+        cy.assertEventWithText("ww1", "id1" , "2");
+        cy.assertEventWithText("ww1", "id2" , "1")
+
+    });
+
+})
+
+
