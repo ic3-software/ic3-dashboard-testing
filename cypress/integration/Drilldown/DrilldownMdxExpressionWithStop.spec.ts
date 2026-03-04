c22016ce2cdb535baad0a790631ee7f7a84dc7e7
david-ic3 <david.alvarez@iccube.com>
2026-03-04 17:18:49 +0100

    add chart test to stop drilldown

--- cypress/integration/Drilldown/DrilldownMdxExpressionWithStop.spec.ts
+++ cypress/integration/Drilldown/DrilldownMdxExpressionWithStop.spec.ts
@@ -2,13 +2,13 @@ export {};
 
 describe("Drilldown/User Select", () => {
 
-    const table1 = "ww0";
-    const table2 = "ww1";
+    const querycount = 4;
 
     beforeEach(() => {
         cy.login();
         cy.openViewerTestReport("Drilldown/Drilldown MDX Expression With Stop");
-        cy.waitForQueryCount(4);
+
+        cy.waitForQueryCount(querycount);
     });
 
     it("pivot table", () => {
@@ -16,11 +16,11 @@ describe("Drilldown/User Select", () => {
         const widgetId = "ww1";
 
         cy.drilldownPivotTableLeftHeader(widgetId, 0, 0 /* All Regions */);
-        cy.waitForQueryCount(5);
+        cy.waitForQueryCount(querycount+1);
         cy.assertPivotTableRowCount(widgetId, 6);
 
         cy.drilldownPivotTableLeftHeader(widgetId, 4, 0 /* 2021 */);
-        cy.waitForQueryCount(6);
+        cy.waitForQueryCount(querycount+2);
         cy.assertPivotTableRowCount(widgetId, 6 + 5);
         cy.assertPivotTableCell(widgetId, 4, 0, "€1,303,900");
         cy.assertPivotTableCell(widgetId, 4 + 1, 0, "€27,500");
@@ -52,5 +52,25 @@ describe("Drilldown/User Select", () => {
         cy.assertDrilldownPivotTableLeftHeader(widgetId, 3, 0 /* 2021 */);
         cy.assertDrilldownPivotTableLeftHeader(widgetId, 4, 0 /* 2022 */);
 
+    });
+
+    it("chart 3", () => {
+
+        const widgetId = "ww3";
+
+        // no drilldown
+        cy.getWidget(widgetId)
+            .find(`svg g[role='group'] g[role='menuitem']:nth-child(2)`)
+            .click({force: true})
+
+        cy.wait(1000);
+        cy.waitForQueryCount(querycount);
+
+        // with drilldown
+        cy.getWidget(widgetId)
+            .find(`svg g[role='group'] g[role='menuitem']:nth-child(1)`)
+            .click({force: true})
+
+        cy.waitForQueryCount(querycount+1);
     });
 });
