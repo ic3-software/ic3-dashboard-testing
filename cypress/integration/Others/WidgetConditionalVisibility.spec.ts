a0a76515f1a8e7b9e67f0d0f1d86b382fd68e4bc
david-ic3 <david.alvarez@iccube.com>
2026-06-08 16:18:30 +0200

    change test according to dashboard

--- cypress/integration/Others/WidgetConditionalVisibility.spec.ts
+++ cypress/integration/Others/WidgetConditionalVisibility.spec.ts
@@ -7,25 +7,25 @@ describe("Others/Widget Conditional Visibility", () => {
     it("Viewer", () => {
 
         cy.openViewerTestReport("Others/Widget Conditional Visibility");
-        cy.waitForQueryCount(3);
+        cy.waitForQueryCount(5);
 
         cy.assertWidgetVisible("ww1", true)
         cy.selectDropdownFromInput("ww0", "2010")
-        cy.assertWidgetInvisible("ww1")
+        cy.assertWidgetInvisible("ww2")
 
-        cy.selectDropdownFromInput("ww0", "2009")
-        cy.assertWidgetVisible("ww1",true)
+        cy.selectDropdownFromInput("ww0", "2011")
+        cy.assertWidgetVisible("ww2",true)
     })
 
     it("Editor", () => {
 
         cy.openEditorTestReport("Others/Widget Conditional Visibility");
-        cy.waitForQueryCount(3);
+        cy.waitForQueryCount(5);
 
-        cy.assertWidgetVisible("ww1", true)
+        cy.assertWidgetVisible("ww2", true)
         cy.selectDropdownFromInput("ww0", "2010")
-        cy.get(".MuiAlert-message").contains("ww1")
-        cy.assertWidgetVisible("ww1", true)
+        cy.get(".MuiAlert-message").contains("ww2")
+        cy.assertWidgetVisible("ww2", true)
     })
 
 
