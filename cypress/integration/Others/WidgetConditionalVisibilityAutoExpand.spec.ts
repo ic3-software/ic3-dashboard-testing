81eff188c80957a369c9927a5bebc9a66a9a6f2c
tom-vd-b <tom.vandenberg@inside-vision.com>
2026-06-11 16:03:40 +0200

    visibility changed & auto expand

---
+++ cypress/integration/Others/WidgetConditionalVisibilityAutoExpand.spec.ts
@@ -0,0 +1,23 @@
+describe("Others/Widget Conditional Visibility AutoExpand", () => {
+
+    beforeEach(() => {
+        cy.login();
+    });
+
+    it("Viewer", () => {
+
+        cy.openViewerTestReport("Others/Widget Conditional Visibility AutoExpand");
+        cy.waitForQueryCount(5);
+
+        cy.assertWidgetVisible("ww1", true)
+        cy.assertWidgetDetails(0, "ww2", 0, 0, 559, 147);
+
+
+        // Hides ww1
+        cy.selectDropdownFromInput("ww0", "2008")
+        cy.assertWidgetInvisible("ww1")
+        cy.assertWidgetDetails(0, "ww2", 0, 0, 748, 147);
+
+    })
+
+})
