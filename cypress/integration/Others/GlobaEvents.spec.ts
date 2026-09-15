e49c0097dc374f30e118a23bef0affcfc919f3cf
marc-polizzi <marc.polizzi@iccube.com>
2026-09-15 07:36:59 +0200

    fix test for cypress 16

--- cypress/integration/Others/GlobaEvents.spec.ts
+++ cypress/integration/Others/GlobaEvents.spec.ts
@@ -5,14 +5,18 @@ describe("Others/GlobalEvents", () => {
         cy.openViewerTestReport("Others/GlobalEvents");
     });
 
-    const user = Cypress.env("ic3_user");
-
     it("ww3:Buttons cell_properties cell_ordinal", () => {
-        /*user === "cypress" &&*/ cy.assertEventWithText("ww0", "ic3_userRole", "cypress")
-        cy.assertEventWithText("ww0", "ic3_userName", user)
-        cy.assertEventWithText("ww0", "ic3_environmentName", "icCube Development")
-        cy.assertEventWithText("ww0", "ic3_environmentColor", "#C0E799")
-        cy.assertEventWithText("ww0", "ic3_userTenant", null)
+
+        cy.env(['ic3_user']).then(({ic3_user}) => {
+
+            cy.assertEventWithText("ww0", "ic3_userRole", "cypress")
+            cy.assertEventWithText("ww0", "ic3_userName", ic3_user)
+            cy.assertEventWithText("ww0", "ic3_environmentName", "icCube Development")
+            cy.assertEventWithText("ww0", "ic3_environmentColor", "#C0E799")
+            cy.assertEventWithText("ww0", "ic3_userTenant", null)
+
+        });
+
     })
 
 })
